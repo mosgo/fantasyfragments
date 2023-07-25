2ee0f86922036488737f4b81dc50d17cf38c4501
mosgo <notmosgo@gmail.com>
2023-07-25 11:28:12 +0100

    Improved site theme consistency and edits to RSS.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,10 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function get() {
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'FantasyFragments | Blog',
+    description: 'A blog on computers and linguistics.',
     site: 'https://fantasyfragments.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    customData: `<language>en-gb</language>`,
   });
 }
